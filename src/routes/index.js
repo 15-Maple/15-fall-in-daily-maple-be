33249627ea7ae81cd5be4ce4d7ea145b4fff3e50
HwangGyuri <gyuriyuri2580@gmail.com>
2026-09-06 23:43:34 +0900

    feat(createLog): 로그 생성 API 구현

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import { focusRecordRoutes } from "./focus-records.routes.js";
+import { logRoutes } from "./logs.routes.js";
 
 export const router = express.Router();
 
 router.use("/focus-records", focusRecordRoutes);
 
 // 여기에 라우터들을 추가해 주세요
+router.use("/logs", logRoutes);
