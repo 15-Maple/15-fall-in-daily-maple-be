a94685e1b9d16fccb36e4eacb227b5999c39995e
HwangGyuri <gyuriyuri2580@gmail.com>
2026-09-07 10:09:31 +0900

    feat(createLog): 로그 생성 api 구현

--- src/controllers/logs.controller.js
+++ src/controllers/logs.controller.js
@@ -4,9 +4,10 @@ export const createLog = async (req, res) => {
   // 에러는 Express 5가 캐치해서 에러 미들웨어로 던져주기 때문에 특별한 처리가 없다면 try catch를 사용하지 않아도 됨
   const log = await logService.createLog(req.body);
 
-  // 응답 예시입니다.
-  res.status(200).json({
+  // 응답
+  res.status(201).json({
     success: true,
+    message: "로그 생성 성공",
     data: log,
   });
 };
