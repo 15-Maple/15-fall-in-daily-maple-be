140e2090c84b4c82546db8673ce2ed17e28bfa5c
apple <apple@appleui-MacBookAir.local>
2026-09-16 17:02:03 +0900

    fix(home): log정보 불러오기에서 패스워드정보 제외

--- src/services/home.service.js
+++ src/services/home.service.js
@@ -1,7 +1,16 @@
 import { prisma } from "#db";
 
 export const getHomeLogs = async () => {
-  const logs = await prisma.log.findMany();
-
-  return logs;
+  return prisma.log.findMany({
+    select: {
+      id: true,
+      name: true,
+      nickname: true,
+      description: true,
+      background: true,
+      points: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
 };
